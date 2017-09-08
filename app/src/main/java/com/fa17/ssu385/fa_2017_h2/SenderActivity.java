a9e30d16e55ec2a5cb9acfdbea752654a9fcabf4
student <student@ssu-cs-dar25-167-144.sonoma.edu>
2017-09-07 20:08:58 -0700

    completed homework

--- app/src/main/java/com/fa17/ssu385/fa_2017_h2/SenderActivity.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h2/SenderActivity.java
@@ -1,12 +1,15 @@
 package com.fa17.ssu385.fa_2017_h2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SenderActivity extends AppCompatActivity {
     private Button sendButton;
+    private EditText senderTextEntry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +17,7 @@ public class SenderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sender);
 
         sendButton = (Button)findViewById(R.id.send_button);
-
+        senderTextEntry = (EditText)findViewById(R.id.sender_text_entry);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,6 +28,13 @@ public class SenderActivity extends AppCompatActivity {
     }
 
     private void processTextForNavigation() {
-
+        if(senderTextEntry != null) {
+            String message = senderTextEntry.getText().toString();
+            if(message.length() != 0) {
+                Intent messageIntent = new Intent(this, ReceiverActivity.class);
+                messageIntent.putExtra("MESSAGE", message);
+                startActivity(messageIntent);
+            }
+        }
     }
 }
