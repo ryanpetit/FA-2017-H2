62176ac190937eb57d5f794d1b9086b9f009d967
Jeffrey Fisher <jeff@bam-043374-jf.local>
2017-09-07 18:30:52 -0700

    added Button handler to match lab instructions

--- app/src/main/java/com/fa17/ssu385/fa_2017_h2/SenderActivity.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h2/SenderActivity.java
@@ -2,12 +2,29 @@ package com.fa17.ssu385.fa_2017_h2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SenderActivity extends AppCompatActivity {
+    private Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sender);
+
+        sendButton = (Button)findViewById(R.id.send_button);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                processTextForNavigation();
+            }
+        });
+
+    }
+
+    private void processTextForNavigation() {
+
     }
 }
