f0779f6390702fc50bb0db0eea3b9b0d2c959c7b
student <student@ssu-cs-dar28-167-219.sonoma.edu>
2017-09-14 18:12:51 -0700

    first commit

---
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h2/ReceiverActivity.java
@@ -0,0 +1,23 @@
+package com.fa17.ssu385.fa_2017_h2;
+//Joshua Hernandez
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ReceiverActivity extends AppCompatActivity {
+
+    private TextView messageText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receiver);
+        messageText = (TextView)findViewById(R.id.message_text);
+        String retrievedMessage = null;
+        if(getIntent().hasExtra("MESSAGE")){
+            retrievedMessage = getIntent().getStringExtra("MESSAGE");
+            messageText.setText(retrievedMessage);
+        }
+    }
+}
